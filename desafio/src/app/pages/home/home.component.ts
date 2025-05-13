f5cb3683722907eae79d0185cdc4e74b05320bed
BRUNO SANTOS FERNANDES <019.689502@universidadesenaicimatec.edu.br>
2025-05-13 09:16:42 -0300

    Add Home CSS

--- desafio/src/app/pages/home/home.component.ts
+++ desafio/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../components/menu/menu.component';
 import { BoasVindasComponent } from '../../components/boas-vindas/boas-vindas.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [MenuComponent, BoasVindasComponent],
+  imports: [MenuComponent, BoasVindasComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
