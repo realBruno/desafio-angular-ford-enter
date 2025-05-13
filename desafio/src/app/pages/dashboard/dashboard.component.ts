4c5019f2d49d47a621d7cfacd8db75aa48a0dce8
BRUNO SANTOS FERNANDES <019.689502@universidadesenaicimatec.edu.br>
2025-05-13 10:01:49 -0300

    Start Dashboard

--- desafio/src/app/pages/dashboard/dashboard.component.ts
+++ desafio/src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../components/menu/menu.component';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MenuComponent],
+  imports: [MenuComponent, CardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
